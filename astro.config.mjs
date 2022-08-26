d8d6ff8deec91f4ccb965cedbbf4e1adf5e08a30
Owen Haupt <owenshaupt@gmail.com>
2022-08-25 17:17:04 -0700

    updated footer, left comments, typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://owenshaupt.github.io',
-	base: '/xyz',
+	base: '/xyz', // this eventually goes away
 	integrations: [mdx(), sitemap()],
 });
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Perosnal Effects';
+export const SITE_TITLE = 'Personal Effects';
 export const SITE_DESCRIPTION = 'Personal Effects';
