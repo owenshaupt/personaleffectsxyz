37b240c152bd581f597d211c9bf2f2034cdd11f6
Owen Haupt <owenshaupt@gmail.com>
2022-08-25 17:59:33 -0700

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://owenshaupt.github.io',
+	site: 'https://www.personal.effects.xyz',
 	integrations: [mdx(), sitemap()],
 });
