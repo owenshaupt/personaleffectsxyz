f85481883e1d0a49a4cc85d892253238abc92b36
Owen Haupt <owenshaupt@gmail.com>
2022-09-06 18:36:46 -0700

    cleanup, node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.personal.effects.xyz',
   integrations: [mdx(), sitemap(), react()],
+  output: 'server',
+  adapter: node(),
 });
